5fc50903bc6ec5e078a6b9e4105d3d275f7ab428
759388 <759388@elf7it218955.ACDM.DS.SAIT.CA>
2018-10-01 20:14:28 -0600

    Week 04 Lab Created Domain Java Classes Created and completed the necessary Java classes in the domain package to create and check the username and password.

--- src/java/domain/User.java
+++ src/java/domain/User.java
@@ -5,11 +5,45 @@
  */
 package domain;
 
+import java.io.Serializable;
 /**
  *
  * @author 759388
  */
 public class User 
 {
-    
+    private String username;
+    private String password;
+
+    public User() 
+    {
+        username = "";
+        password = "";
+    }
+
+    public User(String username, String password) 
+    {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() 
+    {
+        return username;
+    }
+
+    public void setUsername(String username) 
+    {
+        this.username = username;
+    }
+
+    public String getPassword() 
+    {
+        return password;
+    }
+
+    public void setPassword(String password) 
+    {
+        this.password = password;
+    }
 }
--- src/java/domain/UserService.java
+++ src/java/domain/UserService.java
@@ -5,11 +5,28 @@
  */
 package domain;
 
+import java.io.Serializable;
 /**
  *
  * @author 759388
  */
 public class UserService 
 {
-    
+    public User login(String username, String password)
+    {
+        User adam = new User("adam","password");
+        User betty = new User("betty", "password");
+        
+        if(username.equals(adam.getUsername()) && password.equals(adam.getPassword()))
+        {
+            adam.setPassword(null);
+            return adam;
+        }
+        else if(username.equals(betty.getUsername()) && password.equals(betty.getPassword()))
+        {
+            betty.setPassword(null);
+            return betty;
+        }
+        return null;
+    }
 }
